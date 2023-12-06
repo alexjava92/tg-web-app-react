7249c0a34ee18a50d4112903af5de0717c272cf6
alexjava92 <autoexpert37@yandex.ru>
2023-12-06 11:11:22 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+
+const products = [
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Джинсы', price: 1000, description: 'Синего цвета, прямые'},
+    {id: '3', title: 'Джинсы', price: 12000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Джинсы', price: 3000, description: 'Синего цвета, прямые'},
+    {id: '5', title: 'Джинсы', price: 5555, description: 'Синего цвета, прямые'},
+]
+
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
 
 const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg} = useTelegram();
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        }else {
+            newItems = [...addedItems, product];
+        }
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide()
+        }else {
+            tg.MainButton.show()
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
     return (
-        <div>
-            ProductList
+        <div className={'list'}>
+            {products.map(item => {
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            })}
         </div>
     );
 };
