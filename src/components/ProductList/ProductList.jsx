b52005bedb160be9eb3ad3c7226798f5a1fb7ec5
alexjava92 <autoexpert37@yandex.ru>
2023-12-06 15:16:16 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -50,7 +50,7 @@ const ProductList = () => {
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [onSendData])
+    }, [])
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
