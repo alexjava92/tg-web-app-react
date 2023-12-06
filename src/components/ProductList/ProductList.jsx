f87bdfe6508d0ec0ecd37f1fa5f27be4239a630a
alexjava92 <autoexpert37@yandex.ru>
2023-12-06 13:26:38 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -27,7 +27,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://192.168.0.70:8080', {
+        fetch('http://192.168.0.70:8080/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
