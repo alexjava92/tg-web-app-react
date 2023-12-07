b31d72e85955378746a817704fcb74a15cc52f84
alexjava92 <autoexpert37@yandex.ru>
2023-12-07 11:38:08 +0300

    init

--- src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
+++ src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './BitcoinAddress.css'
 
 const address = 'bc1q84xfmq02lz9tqlndq24gqrxydgtya8er2dxxjf'
 
 const BitcoinAddress = () => {
     return (
-        <div>
+        <div className={'body'}>
             <h3>Новый адрес биткоина:</h3>
             <p>{address}</p>
         </div>
