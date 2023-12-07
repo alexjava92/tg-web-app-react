2eaf4581b924bd92b24773ab25cff959af429fb1
alexjava92 <autoexpert37@yandex.ru>
2023-12-07 16:06:49 +0300

    init

--- src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
+++ src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
@@ -27,6 +27,10 @@ const BitcoinAddress = () => {
                     closeOnClick: true,
                     pauseOnHover: true,
                     draggable: true,
+                    style: {
+                        background: 'var(--tg-theme-secondary-bg-color)', // Задайте цвет фона
+                        color: 'var(--tg-theme-text-color)', // Задайте цвет текста
+                    },
                 });
             })
             .catch((error) => {
