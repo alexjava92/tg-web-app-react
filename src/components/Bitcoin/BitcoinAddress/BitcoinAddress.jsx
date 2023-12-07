8f359e4603e8cb3c85d50b0ed22081ba7963c521
alexjava92 <autoexpert37@yandex.ru>
2023-12-07 11:55:50 +0300

    init

--- src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
+++ src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClipboardJS from 'clipboard';
 import './BitcoinAddress.css';
 
 const address = 'bc1q84xfmq02lz9tqlndq24gqrxydgtya8er2dxxjf';
 
 const BitcoinAddress = () => {
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
     const handleCopyAddress = () => {
         const clipboard = new ClipboardJS('.copy-button', {
             text: function () {
@@ -12,12 +14,16 @@ const BitcoinAddress = () => {
             },
         });
 
+        setIsButtonDisabled(true);
+
         clipboard.on('success', function (e) {
             console.log('Address copied to clipboard:', e.text);
+            setIsButtonDisabled(false);
         });
 
         clipboard.on('error', function (e) {
             console.error('Error copying address to clipboard:', e.text);
+            setIsButtonDisabled(false);
         });
 
         clipboard.destroy();
@@ -27,7 +33,11 @@ const BitcoinAddress = () => {
         <div className={'body'}>
             <h3>Новый адрес биткоина:</h3>
             <p>{address}</p>
-            <button className="copy-button" onClick={handleCopyAddress}>
+            <button
+                className="copy-button"
+                onClick={handleCopyAddress}
+                disabled={isButtonDisabled}
+            >
                 Копировать
             </button>
         </div>
