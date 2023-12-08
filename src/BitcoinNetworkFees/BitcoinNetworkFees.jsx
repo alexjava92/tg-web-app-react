ec194f5c96d75fc71fa2850f856cc1db99c71943
alexjava92 <autoexpert37@yandex.ru>
2023-12-08 17:05:43 +0300

    init

--- src/BitcoinNetworkFees/BitcoinNetworkFees.jsx
+++ src/BitcoinNetworkFees/BitcoinNetworkFees.jsx
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { getFees } from './apiGetFees'; // Укажите правильный путь к вашему файлу с API-запросом
+import {useEffect} from "react";
 
 export const BitcoinNetworkFees = ({ onSelect }) => {
     const [fees, setFees] = useState([]);
-    const [selectedCommission, setSelectedCommission] = useState('');
 
     useEffect(() => {
-        // Вызывайте ваш API-запрос при монтировании компонента
-        getFees()
-            .then(data => {
-                // Обновляем состояние с полученными данными
-                setFees(data);
-                // Если еще не выбрана комиссия, выберите первую из полученных
-                if (!selectedCommission && data.length > 0) {
-                    setSelectedCommission(data[0].satPerByte.toString());
-                    onSelect(data[0].satPerByte.toString());
-                }
-            })
-            .catch(error => {
+        const fetchFees = async () => {
+            try {
+                // Получаем данные с сервера
+                const feesData = await getFees();
+
+                // Преобразуем объект в массив
+                const feesArray = Object.entries(feesData).map(([key, value]) => ({
+                    label: key,
+                    value: value,
+                    satPerByte: value,
+                }));
+
+                // Устанавливаем данные в state
+                setFees(feesArray);
+            } catch (error) {
                 console.error('Error fetching fees:', error);
-            });
-    }, [onSelect, selectedCommission]);
+            }
+        };
+
+        fetchFees();
+    }, []);
 
     const handleSelectChange = (e) => {
         const selectedValue = e.target.value;
-        setSelectedCommission(selectedValue);
         onSelect(selectedValue);
     };
 
@@ -32,16 +35,13 @@ export const BitcoinNetworkFees = ({ onSelect }) => {
         <div>
             <label htmlFor="commission">Выберите комиссию:</label>
             <select id="commission" onChange={handleSelectChange}>
-                {Array.isArray(fees) ? (
-                    fees.map((fee) => (
-                        <option key={fee.value} value={fee.value}>
-                            {fee.label} ({fee.satPerByte} sat/b)
-                        </option>
-                    ))
-                ) : (
-                    <option value="default">Нет данных о комиссиях</option>
-                )}
+                {fees.map((fee) => (
+                    <option key={fee.value} value={fee.value}>
+                        {fee.label} ({fee.satPerByte} sat/b)
+                    </option>
+                ))}
             </select>
         </div>
     );
 };
+
