35b66dd89f368c7c3cb6eb810c00f8a0b066dee7
alexjava92 <autoexpert37@yandex.ru>
2023-12-08 10:14:18 +0300

    init

--- src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
+++ src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
@@ -1,13 +1,62 @@
-import React, {useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './BitcoinAddress.css';
 import '../../../GlobalStyle.css';
-import {useTelegram} from '../../../hooks/useTelegram';
-import {useNavigate} from 'react-router-dom';
-import {ToastContainer, toast} from 'react-toastify';
+import { useTelegram } from '../../../hooks/useTelegram';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import {useFetchBitcoinAddress} from "../../../api/useFetchBitcoinAddress";
-import {useCopyToClipboard} from "../../../hooks/useCopyToClipboard";
+
+// Хук для работы с сервером
+const useFetchBitcoinAddress = (chatId, setAddress) => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://e4f6-62-33-234-17.ngrok-free.app/web-new-bitcoin-address', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ chatId }),
+                });
+
+                if (response.ok) {
+                    const responseData = await response.json();
+                    const newAddress = responseData.address.newAddress;
+                    setAddress(newAddress);
+                    console.log('Получен адрес:', newAddress);
+                } else {
+                    console.error('Server returned an error:', response.status);
+                }
+            } catch (error) {
+                console.error('Error fetching data from the server:', error);
+            }
+        };
+
+        fetchData();
+    }, [chatId, setAddress]);
+};
+
+// Хук для работы с уведомлениями
+const useCopyToClipboard = (text) => {
+    const handleCopyAddress = useCallback(() => {
+        // Показываем уведомление об успешном копировании
+        toast.success('Адрес скопирован', {
+            position: 'top-center',
+            autoClose: 300, // Закрытие через 3 секунды
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            style: {
+                background: 'var(--tg-theme-secondary-bg-color)', // Задайте цвет фона
+                color: 'var(--tg-theme-text-color)', // Задайте цвет текста
+            },
+        });
+    }, []);
+
+    return { handleCopyAddress };
+};
 
 const BitcoinAddress = () => {
     const { tg, chatId } = useTelegram();
