4e25b5a1e95a2cc7a766e2108ca0039cf72ef6a6
alexjava92 <autoexpert37@yandex.ru>
2023-12-08 17:34:25 +0300

    init

--- src/BitcoinNetworkFees/BitcoinNetworkFees.jsx
+++ src/BitcoinNetworkFees/BitcoinNetworkFees.jsx
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from "react";
 import {getFees} from "./apiGetFees";
-
 
 export const BitcoinNetworkFees = ({ onSelect }) => {
     const [fees, setFees] = useState([]);
-    const [selectedFee, setSelectedFee] = useState('');
 
     useEffect(() => {
         const fetchFees = async () => {
             try {
+                // Получаем данные с сервера
                 const feesData = await getFees();
-                // Выбираем только необходимые комиссии и переименовываем их
-                const filteredFees = {
-                    Приоритет: feesData.fastestFee,
-                    Средняя: feesData.hourFee,
-                    Обычная: feesData.economyFee,
-                };
-                setFees(Object.values(filteredFees));
-                setSelectedFee(Object.values(filteredFees)[0]); // Устанавливаем первое значение по умолчанию
+
+                // Преобразуем объект в массив
+                const feesArray = Object.entries(feesData).map(([key, value]) => ({
+                    label: key,
+                    value: value,
+                    satPerByte: value,
+                }));
+
+                // Устанавливаем данные в state
+                setFees(feesArray);
             } catch (error) {
                 console.error('Error fetching fees:', error);
             }
@@ -28,20 +29,20 @@ export const BitcoinNetworkFees = ({ onSelect }) => {
 
     const handleSelectChange = (e) => {
         const selectedValue = e.target.value;
-        setSelectedFee(selectedValue);
-        onSelect(fees.find(([key]) => key === selectedKey)[1]);
+        onSelect(selectedValue);
     };
 
     return (
         <div>
-            <label htmlFor="fee">Выберите комиссию:</label>
-            <select id="fee" value={selectedFee} onChange={handleSelectChange}>
-                {fees.map(([key, value], index) => (
-                    <option key={index} value={key}>
-                        {key} - {value} sat/b
+            <label htmlFor="commission">Выберите комиссию:</label>
+            <select id="commission" onChange={handleSelectChange}>
+                {fees.map((fee) => (
+                    <option key={fee.value} value={fee.value}>
+                        {fee.label} {fee.satPerByte} sat/b
                     </option>
                 ))}
             </select>
         </div>
     );
 };
+
