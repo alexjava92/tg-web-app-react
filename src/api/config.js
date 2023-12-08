65cdfcefb8c9858b9a43d6eccf64ff7c5ce9df90
alexjava92 <autoexpert37@yandex.ru>
2023-12-08 10:35:22 +0300

    init

---
+++ src/api/config.js
@@ -0,0 +1,4 @@
+export const config = {
+    apiBaseUrl: 'https://e4f6-62-33-234-17.ngrok-free.app',
+};
+
--- src/api/useFetchBitcoinAddress.js
+++ src/api/useFetchBitcoinAddress.js
@@ -1,11 +1,13 @@
 // Хук для работы с сервером
 import {useEffect} from "react";
+import {config} from "./config";
 
+const url = config.apiBaseUrl;
 export const useFetchBitcoinAddress = (chatId, setAddress) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('https://e4f6-62-33-234-17.ngrok-free.app/web-new-bitcoin-address', {
+                const response = await fetch(`${url}/web-new-bitcoin-address`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
