42e3bed25b6d3a36ffdd3a805e98c6f229f7551b
alexjava92 <autoexpert37@yandex.ru>
2023-12-08 10:22:32 +0300

    init

--- src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
+++ src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './BitcoinAddress.css';
 import '../../../GlobalStyle.css';
 import { useTelegram } from '../../../hooks/useTelegram';
@@ -6,27 +6,19 @@ import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import { useFetchBitcoinAddress } from '../../../api/useFetchBitcoinAddress';
-import { useCopyToClipboard } from '../../../hooks/useCopyToClipboard';
+import {useFetchBitcoinAddress} from "../../../api/useFetchBitcoinAddress";
+import {useCopyToClipboard} from "../../../hooks/useCopyToClipboard";
+
+
 
 const BitcoinAddress = () => {
     const { tg, chatId } = useTelegram();
     const backButton = tg.BackButton;
     const navigate = useNavigate();
     const [address, setAddress] = useState('');
-    const [loading, setLoading] = useState(true);
 
     // Используем хуки для работы с сервером и уведомлениями
-    useFetchBitcoinAddress(
-        chatId,
-        (newAddress) => {
-            setAddress(newAddress);
-            setLoading(false);
-        },
-        () => {
-            setLoading(false);
-        }
-    );
+    useFetchBitcoinAddress(chatId, setAddress);
     const { handleCopyAddress } = useCopyToClipboard();
 
     useEffect(() => {
@@ -41,13 +33,7 @@ const BitcoinAddress = () => {
     return (
         <div className={'body'}>
             <h3>Новый адрес биткоина:</h3>
-            {loading ? (
-                <p>Loading...</p> // Ваша анимация загрузки может быть добавлена здесь
-            ) : (
-                <p>
-                    <code style={{ fontFamily: 'monospace' }}>{address}</code>
-                </p>
-            )}
+            <p><code style={{ fontFamily: 'monospace' }}>{address}</code></p>
             <CopyToClipboard text={address}>
                 <button className={'button'} onClick={handleCopyAddress}>
                     Скопировать адрес bitcoin
