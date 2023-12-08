f35477e85a629a29c82adead34bebace09fc3875
alexjava92 <autoexpert37@yandex.ru>
2023-12-08 17:13:23 +0300

    init

--- src/BitcoinNetworkFees/BitcoinNetworkFees.jsx
+++ src/BitcoinNetworkFees/BitcoinNetworkFees.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import {getFees} from "./apiGetFees";
 
 export const BitcoinNetworkFees = ({ onSelect }) => {
     const [fees, setFees] = useState([]);
