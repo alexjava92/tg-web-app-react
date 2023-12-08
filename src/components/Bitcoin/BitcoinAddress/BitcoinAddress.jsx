1767e056fd08fcdb4d14617448038ab09cab1747
alexjava92 <autoexpert37@yandex.ru>
2023-12-08 10:02:51 +0300

    init

--- src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
+++ src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
@@ -1,21 +1,19 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './BitcoinAddress.css';
 import '../../../GlobalStyle.css';
-import {useTelegram} from '../../../hooks/useTelegram';
-import {useNavigate} from 'react-router-dom';
-import {ToastContainer, toast} from 'react-toastify';
+import { useTelegram } from '../../../hooks/useTelegram';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
-
 const BitcoinAddress = () => {
-    const {tg, chatId} = useTelegram();
+    const { tg, chatId } = useTelegram();
     const backButton = tg.BackButton;
     const navigate = useNavigate();
     const [address, setAddress] = useState('');
 
     const handleCopyAddress = () => {
-
         // Показываем уведомление об успешном копировании
         toast.success('Адрес скопирован', {
             position: 'top-center',
@@ -29,49 +27,55 @@ const BitcoinAddress = () => {
                 color: 'var(--tg-theme-text-color)', // Задайте цвет текста
             },
         });
-
     };
 
-    useEffect(async () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://e4f6-62-33-234-17.ngrok-free.app/web-new-bitcoin-address', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ chatId }),
+                });
 
-        try {
-            const response = await fetch('https://e4f6-62-33-234-17.ngrok-free.app/web-new-bitcoin-address', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({chatId}),
-            });
-
-            if (response.ok) {
-                const responseData = await response.json();
-                const newAddress = responseData.address.newAddress;
-                setAddress(newAddress);
-                console.log('Получен адрес:', newAddress);
-            } else {
-                console.error('Server returned an error:', response.status);
+                if (response.ok) {
+                    const responseData = await response.json();
+                    const newAddress = responseData.address.newAddress;
+                    setAddress(newAddress);
+                    console.log('Получен адрес:', newAddress);
+                } else {
+                    console.error('Server returned an error:', response.status);
+                }
+            } catch (error) {
+                console.error('Error fetching data from the server:', error);
             }
-        } catch (error) {
-            console.error('Error fetching data from the server:', error);
-        }
-    }, [chatId]);
+        };
 
+        fetchData();
+
+        // Показываем кнопку назад после загрузки данных
+        backButton.show();
+
+        return () => {
+            tg.MainButton.offClick(handleCopyAddress);
+            tg.MainButton.hide();
+        };
+    }, [chatId, backButton]);
 
     backButton.onClick(() => {
         navigate(-1);
     });
 
-    backButton.show();
-
-
     return (
         <div className={'body'}>
             <h3>Новый адрес биткоина:</h3>
-            <p><code style={{fontFamily: 'monospace'}}>{address}</code></p>
+            <p><code style={{ fontFamily: 'monospace' }}>{address}</code></p>
             <CopyToClipboard text={address}>
                 <button className={'button'} onClick={handleCopyAddress}>Скопировать адрес bitcoin</button>
             </CopyToClipboard>
-            <ToastContainer/>
+            <ToastContainer />
         </div>
     );
 };
