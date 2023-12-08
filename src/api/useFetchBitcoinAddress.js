29263c2aeec4729055f4590bdffa544d93e56187
alexjava92 <autoexpert37@yandex.ru>
2023-12-08 10:27:23 +0300

    init

--- src/api/useFetchBitcoinAddress.js
+++ src/api/useFetchBitcoinAddress.js
@@ -1,32 +1,31 @@
-// useFetchBitcoinAddress.js
-import { useState } from 'react';
+// Хук для работы с сервером
+import {useEffect} from "react";
 
-export const useFetchBitcoinAddress = () => {
-    const [bitcoinAddress, setBitcoinAddress] = useState('');
+export const useFetchBitcoinAddress = (chatId, setAddress) => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://e4f6-62-33-234-17.ngrok-free.app/web-new-bitcoin-address', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ chatId }),
+                });
 
-    const fetchBitcoinAddress = async (chatId) => {
-        // Реализация запроса на сервер для получения нового адреса
-        const response = await fetch('https://e4f6-62-33-234-17.ngrok-free.app/web-new-bitcoin-address', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ chatId }),
-        });
+                if (response.ok) {
+                    const responseData = await response.json();
+                    const newAddress = responseData.address.newAddress;
+                    setAddress(newAddress);
+                    console.log('Получен адрес:', newAddress);
+                } else {
+                    console.error('Server returned an error:', response.status);
+                }
+            } catch (error) {
+                console.error('Error fetching data from the server:', error);
+            }
+        };
 
-        if (response.ok) {
-            const responseData = await response.json();
-            const newAddress = responseData.address.newAddress;
-            setBitcoinAddress(newAddress);
-            console.log('Получен адрес:', newAddress);
-            return newAddress;
-        } else {
-            console.error('Server returned an error:', response.status);
-            throw new Error('Error fetching data from the server');
-        }
-    };
-
-    return { fetchBitcoinAddress };
+        fetchData();
+    }, [chatId, setAddress]);
 };
-
-
--- src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
+++ src/components/Bitcoin/BitcoinAddress/BitcoinAddress.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './BitcoinAddress.css';
 import '../../../GlobalStyle.css';
 import { useTelegram } from '../../../hooks/useTelegram';
@@ -6,41 +6,25 @@ import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import { useFetchBitcoinAddress } from '../../../api/useFetchBitcoinAddress';
-import { useCopyToClipboard } from '../../../hooks/useCopyToClipboard';
+import {useFetchBitcoinAddress} from "../../../api/useFetchBitcoinAddress";
+import {useCopyToClipboard} from "../../../hooks/useCopyToClipboard";
+
+
 
 const BitcoinAddress = () => {
     const { tg, chatId } = useTelegram();
     const backButton = tg.BackButton;
     const navigate = useNavigate();
     const [address, setAddress] = useState('');
-    const [loading, setLoading] = useState(true);
 
-    const { fetchBitcoinAddress } = useFetchBitcoinAddress();
-
+    // Используем хуки для работы с сервером и уведомлениями
+    useFetchBitcoinAddress(chatId, setAddress);
     const { handleCopyAddress } = useCopyToClipboard();
 
     useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const newAddress = await fetchBitcoinAddress(chatId);
-                setAddress(newAddress);
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching data from the server:', error);
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-
+        // Показываем кнопку назад после загрузки данных
         backButton.show();
-
-        return () => {
-            backButton.hide();
-        };
-    }, [chatId, backButton, fetchBitcoinAddress]);
+    }, [backButton]);
 
     backButton.onClick(() => {
         navigate(-1);
@@ -49,13 +33,7 @@ const BitcoinAddress = () => {
     return (
         <div className={'body'}>
             <h3>Новый адрес биткоина:</h3>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <p>
-                    <code style={{ fontFamily: 'monospace' }}>{address}</code>
-                </p>
-            )}
+            <p><code style={{ fontFamily: 'monospace' }}>{address}</code></p>
             <CopyToClipboard text={address}>
                 <button className={'button'} onClick={handleCopyAddress}>
                     Скопировать адрес bitcoin
