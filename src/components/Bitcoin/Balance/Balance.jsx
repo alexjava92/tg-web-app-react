03c65bff026b7c8e5872a7c562cc37895e422a2c
alexjava92 <autoexpert37@yandex.ru>
2023-12-11 09:33:09 +0300

    init

--- src/components/Bitcoin/Balance/Balance.jsx
+++ src/components/Bitcoin/Balance/Balance.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Balance.css'
+import '../../../App.css'
 
 export const Balance = ({ balanceToBtc, balanceToRub }) => {
     return (
-        <div>
+        <div className={'body_second'}>
             <h2 className={'h2'}>Баланс:</h2>
             <div>{balanceToBtc} BTC</div>
             <div>{balanceToRub} ₽</div>
