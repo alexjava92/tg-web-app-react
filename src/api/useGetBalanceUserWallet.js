07580a0e30a7ef5dc35f900ee2d7c9cde41b1099
alexjava92 <autoexpert37@yandex.ru>
2023-12-11 15:17:08 +0300

    init

--- src/api/useGetBalanceUserWallet.js
+++ src/api/useGetBalanceUserWallet.js
@@ -1,11 +1,13 @@
-// Хук для работы с сервером
 import { useEffect } from "react";
 import { config } from "./config";
 
 const url = config.apiBaseUrl;
+
 export const useGetBalanceUserWallet = (chatId, setBalance, onLoaded) => {
     useEffect(() => {
         const fetchData = async () => {
+            let responseStatus = 0; // Добавляем переменную для отслеживания статуса ответа
+
             try {
                 const response = await fetch(`${url}/web-new-balance-user-wallet`, {
                     method: 'POST',
@@ -15,25 +17,26 @@ export const useGetBalanceUserWallet = (chatId, setBalance, onLoaded) => {
                     body: JSON.stringify({ chatId }),
                 });
 
+                responseStatus = response.status; // Обновляем статус ответа
+
                 if (response.ok) {
                     const responseData = await response.json();
                     const newBalance = responseData.balance;
                     setBalance(newBalance.balance);
                     console.log('Получен баланс:', newBalance.balance);
-                    onLoaded(); // Вызываем onLoaded после успешной установки баланса
                 } else {
                     console.error('Server returned an error:', response.status);
                 }
             } catch (error) {
                 console.error('Error fetching data from the server:', error);
             } finally {
-                if (!response.ok) {
-                    onLoaded(); // Вызываем onLoaded даже если запрос завершился ошибкой
+                if (responseStatus !== 200) {
+                    console.log('Запрос завершился с ошибкой:', responseStatus);
                 }
+                onLoaded(); // Вызываем onLoaded в блоке finally
             }
         };
 
         fetchData();
-    }, [chatId, setBalance]);
+    }, [chatId, setBalance, onLoaded]); // Добавляем onLoaded в массив зависимостей
 };
-
