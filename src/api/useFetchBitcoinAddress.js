6d74f6cd9ed101dda43a1c490370666f41761b67
alexjava92 <autoexpert37@yandex.ru>
2023-12-11 15:21:39 +0300

    init

--- src/api/useFetchBitcoinAddress.js
+++ src/api/useFetchBitcoinAddress.js
@@ -1,11 +1,13 @@
-// Хук для работы с сервером
-import {useEffect} from "react";
-import {config} from "./config";
+import { useEffect } from "react";
+import { config } from "./config";
 
 const url = config.apiBaseUrl;
+
 export const useFetchBitcoinAddress = (chatId, setAddress, onLoaded) => {
     useEffect(() => {
         const fetchData = async () => {
+            let requestFailed = false; // Добавляем флаг, указывающий на ошибку
+
             try {
                 const response = await fetch(`${url}/web-new-bitcoin-address`, {
                     method: 'POST',
@@ -20,16 +22,18 @@ export const useFetchBitcoinAddress = (chatId, setAddress, onLoaded) => {
                     const newAddress = responseData.address.newAddress;
                     setAddress(newAddress);
                     console.log('Получен адрес:', newAddress);
-                    onLoaded();
                 } else {
                     console.error('Server returned an error:', response.status);
+                    requestFailed = true; // Устанавливаем флаг ошибки
                 }
             } catch (error) {
                 console.error('Error fetching data from the server:', error);
+                requestFailed = true; // Устанавливаем флаг ошибки
             } finally {
-                if (!response.ok) {
-                    onLoaded(); // Вызываем onLoaded даже если запрос завершился ошибкой
+                if (requestFailed) {
+                    console.log('Запрос завершился с ошибкой');
                 }
+                onLoaded(); // Вызываем onLoaded в блоке finally
             }
         };
 
