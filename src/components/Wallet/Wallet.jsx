056893e9723e100f95b28156945933a637a15e73
alexjava92 <autoexpert37@yandex.ru>
2023-12-12 16:07:34 +0300

    init

--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -32,7 +32,6 @@ const Wallet = () => {
                 { id: 'ok_button', type: 'ok', text: 'Ок' },
                 { id: 'cancel_button', type: 'cancel', text: 'Отмена' },
                 { id: 'add_button', type: 'default', text: 'Добавить' },
-                { id: 'add_butt', type: 'default', text: 'Добавить' }
             ]
         }, function(result) {
             if (result) {
