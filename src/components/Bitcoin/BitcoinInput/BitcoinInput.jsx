20f913538f63d85f4d40a402620d0d842e1583d0
alexjava92 <autoexpert37@yandex.ru>
2023-12-12 09:15:19 +0300

    init

--- src/components/Bitcoin/BitcoinInput/BitcoinInput.jsx
+++ src/components/Bitcoin/BitcoinInput/BitcoinInput.jsx
@@ -112,7 +112,7 @@ export const BitcoinInput = ({
                     id="bitcoinAddress"
                     value={bitcoinAddress}
                     placeholder="Адрес"
-                    onChange={handleRubAmountChange}
+                    onChange={handleBitcoinAddressChange}
                 />
             </div>
             {canRemove && (
