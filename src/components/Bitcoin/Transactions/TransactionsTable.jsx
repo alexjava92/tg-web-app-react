29d1f53a10c444f7f0eb7b198accd67a6dbbdc2c
alexjava92 <autoexpert37@yandex.ru>
2023-12-14 20:06:25 +0300

    init

--- src/components/Bitcoin/Transactions/TransactionsTable.jsx
+++ src/components/Bitcoin/Transactions/TransactionsTable.jsx
@@ -2,36 +2,36 @@ import React from 'react';
 import './TransactionsTable.css'
 import '../../../App.css'
 
-const TransactionCard = ({ transaction }) => {
+const TransactionCard = ({transaction}) => {
     return (
-        <div>
-            <div>
-                <div className={'transaction-id'}>
-                    TXID: {transaction.txid}
-                </div>
 
-                <div>
-                    Senders: {transaction.senders.join(', ')}
-                </div>
-                <div>
-                    Recipients: {transaction.recipients.join(', ')}
-                </div>
-                <div>
-                    Confirmed: {transaction.confirmed ? 'Yes' : 'No'}
-                </div>
-                <div>
-                    Block Time: {transaction.blockTime}
-                </div>
+        <div  className="body_second">
+            <div className={'transaction-id'}>
+                TXID: {transaction.txid}
+            </div>
+
+            <div>
+                Senders: {transaction.senders.join(', ')}
+            </div>
+            <div>
+                Recipients: {transaction.recipients.join(', ')}
+            </div>
+            <div>
+                Confirmed: {transaction.confirmed ? 'Yes' : 'No'}
+            </div>
+            <div>
+                Block Time: {transaction.blockTime}
             </div>
         </div>
+
     );
 };
 
-export const TransactionsList = ({ transactions }) => {
+export const TransactionsList = ({transactions}) => {
     return (
-        <div className="body_second">
+        <div>
             {transactions.map((transaction, index) => (
-                <TransactionCard key={index} transaction={transaction} />
+                <TransactionCard key={index} transaction={transaction}/>
             ))}
         </div>
     );
