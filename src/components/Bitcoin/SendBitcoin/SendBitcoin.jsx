698806bd282db3e45100fb2ff0c45a114a07e11b
alexjava92 <autoexpert37@yandex.ru>
2023-12-14 09:58:49 +0300

    init

--- src/components/Bitcoin/SendBitcoin/SendBitcoin.jsx
+++ src/components/Bitcoin/SendBitcoin/SendBitcoin.jsx
@@ -122,7 +122,7 @@ export const SendBitcoin = () => {
             if (allInputsValid) {
                 getWeightTransactions(chatId, outputs, setVirtualSize);
             }
-        }, 3000); // Задержка в 1000 миллисекунд
+        }, 5000); // Задержка в 1000 миллисекунд
 
         return () => clearTimeout(handler);
     }, [outputs, allInputsValid]);
