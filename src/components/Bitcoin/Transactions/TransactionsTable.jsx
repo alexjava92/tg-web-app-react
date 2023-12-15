8b977f9392e8fb2b771fe27156724a0a8278bedc
alexjava92 <autoexpert37@yandex.ru>
2023-12-15 22:00:55 +0300

    init

--- src/components/Bitcoin/Transactions/TransactionsTable.jsx
+++ src/components/Bitcoin/Transactions/TransactionsTable.jsx
@@ -53,7 +53,7 @@ const TransactionCard = ({transaction}) => {
                     <div>{transaction.blockTime}</div>
                 </div>
                 <div className={`transaction-amount ${amountClass}`}>
-                    <div>{amount}</div>
+                    <div className={`transaction-amount ${amountClass}`}>{amount}</div>
                     {amountInRub !== null && <div className={'amount_rub'}>{amountInRub} RUB</div>}
                 </div>
             </div>
