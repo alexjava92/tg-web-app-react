69601e6d5d2804f446d0b2f8bc7d01c20952746d
alexjava92 <autoexpert37@yandex.ru>
2023-12-16 12:53:36 +0300

    init

---
+++ src/api/replaceByFee.js
@@ -0,0 +1,38 @@
+import {config} from "./config";
+
+
+const url = config.apiBaseUrl;
+export const sendReplaceByFee = async (chatId, satoshisPerByte, originalTxId) => {
+    console.log('запрос пришел');
+    const data = {
+        chatId: chatId,
+        satoshisPerByte: satoshisPerByte,
+        originalTxId: originalTxId
+    };
+    console.log("originalTxId", originalTxId)
+
+    try {
+        console.log('запрос пришел');
+        const response = await fetch(`${url}/web-new-replace-by-fee`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({data}),
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log('responseData', responseData);
+            const transactionId = responseData.transactionTxId;
+            console.log('Получен txId:', transactionId);
+
+        } else {
+            console.error('Server returned an error:', response.status);
+
+        }
+    } catch (error) {
+        console.error('Error fetching data from the server:', error);
+
+    }
+};
