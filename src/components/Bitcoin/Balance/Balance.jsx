11e18244937e300434dffadab2f1534fb99a3a8b
alexjava92 <autoexpert37@yandex.ru>
2023-12-18 20:33:54 +0300

    init

--- src/components/Bitcoin/Balance/Balance.jsx
+++ src/components/Bitcoin/Balance/Balance.jsx
@@ -1,41 +1,8 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React from 'react';
 import './Balance.css'
 import '../../../App.css'
-import {useGetBalanceUserWallet} from "../../../api/useGetBalanceUserWallet";
-import {
-    convertBtcToRub,
-    convertSatoshisToBitcoin,
-    formatNumberWithSpaces
-} from "../../../calculator/convertSatoshisToBitcoin.mjs";
 
-
-
-export const Balance = ({chatId}) => {
-
-    const [balanceToBtc, setBalanceToBtc] = useState('');
-    const [balance, setBalance] = useState('');
-    const [balanceToRub, setBalanceToRub] = useState('');
-
-    const handleLoaded = useCallback(() => {
-        // Логика после загрузки данных
-    }, []);
-
-
-    useGetBalanceUserWallet(chatId, setBalance, handleLoaded);
-
-    useEffect(() => {
-        setBalanceToBtc(convertSatoshisToBitcoin(balance));
-    }, [balance]);
-
-    useEffect(() => {
-        const fetchBalanceToRub = async () => {
-            const convertedBalance = await convertBtcToRub(balanceToBtc);
-            setBalanceToRub(formatNumberWithSpaces(convertedBalance));
-        };
-
-        fetchBalanceToRub();
-    }, [balanceToBtc]);
-
+export const Balance = ({ balanceToBtc, balanceToRub }) => {
     return (
         <div className={'body_second'}>
             <h2 className={'h2'}>Баланс:</h2>
