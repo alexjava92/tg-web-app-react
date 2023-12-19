a1b46d88430147c1ac8c42f990f175e34b3166b7
alexjava92 <autoexpert37@yandex.ru>
2023-12-19 21:12:47 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import {Route, Routes} from "react-router-dom";
-import ProductList from "./components/ProductList/ProductList";
-import Form from "./components/Form/Form";
 import Wallet from "./components/Wallet/Wallet";
 import BitcoinAddress from "./components/Bitcoin/BitcoinAddress/BitcoinAddress";
 import {SendBitcoin} from "./components/Bitcoin/SendBitcoin/SendBitcoin";
@@ -22,8 +20,6 @@ function App() {
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
             </head>
             <Routes>
-                <Route index element={<ProductList/>}/>
-                <Route path={'/form'} element={<Form/>}/>
                 <Route path={'/wallet'} element={<Wallet/>}/>
                 <Route path={'/bitcoin-address'} element={<BitcoinAddress/>}/>
                 <Route path={'/send-bitcoin'} element={<SendBitcoin/>}/>
