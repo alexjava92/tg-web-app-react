4ad70f31edfb2d749e38a85b3075359b3b3939ce
alexjava92 <autoexpert37@yandex.ru>
2023-12-19 15:11:49 +0300

    init

--- src/components/Bitcoin/Balance/Balance.jsx
+++ src/components/Bitcoin/Balance/Balance.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import './Balance.css'
-import '../../../App.css'
+import React, { useState, useEffect } from 'react';
+import './Balance.css';
 
+export const Balance = ({ balanceToBtc, balanceToRub }) => {
+    const [animatedBtc, setAnimatedBtc] = useState(0);
+    const [animatedRub, setAnimatedRub] = useState(0);
 
-export const Balance = ({balanceToBtc, balanceToRub}) => {
+    const animateValue = (start, end, duration, setFunction) => {
+        let startTimestamp = null;
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            setFunction(start + progress * (end - start));
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            } else {
+                setFunction(end); // После завершения анимации устанавливаем конечное значение
+            }
+        };
+        window.requestAnimationFrame(step);
+    };
+
+    useEffect(() => {
+        // Удаляем пробелы перед преобразованием строки в число
+        const btcValue = parseFloat(balanceToBtc.replace(/ /g, '')) || 0;
+        const rubValue = parseFloat(balanceToRub.replace(/ /g, '')) || 0;
+        animateValue(0, btcValue, 1000, setAnimatedBtc);
+        animateValue(0, rubValue, 1000, setAnimatedRub);
+    }, [balanceToBtc, balanceToRub]);
+
+    // Округляем только для отображения
+    const displayBtc = animatedBtc.toFixed(8);
+    const displayRub = animatedRub.toFixed(2);
+
     return (
-        <div className={'body_second'}>
-            {/*<h2 className={'h2'}>Баланс:</h2>*/}
-            <div className={'balance'}>{balanceToBtc} BTC</div>
-            <div className={'balance'}>{balanceToRub} ₽</div>
+        <div className='body_second'>
+            <div className='balance'>{displayBtc} BTC</div>
+            <div className='balance'>{displayRub} ₽</div>
         </div>
     );
 };
