f5efc03196942916b71677caf659745376767a50
alexjava92 <autoexpert37@yandex.ru>
2023-12-19 15:03:45 +0300

    init

--- src/components/Bitcoin/Balance/Balance.jsx
+++ src/components/Bitcoin/Balance/Balance.jsx
@@ -1,40 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Balance.css';
+import CountUp from 'react-countup'; // Default export, no curly braces needed
 
 export const Balance = ({ balanceToBtc, balanceToRub }) => {
-    const [animatedBtc, setAnimatedBtc] = useState(0);
-    const [animatedRub, setAnimatedRub] = useState(0);
-
-    const animateValue = (start, end, duration, setFunction) => {
-        let startTimestamp = null;
-        const step = (timestamp) => {
-            if (!startTimestamp) startTimestamp = timestamp;
-            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-            setFunction(start + progress * (end - start));
-            if (progress < 1) {
-                window.requestAnimationFrame(step);
-            } else {
-                setFunction(end); // После завершения анимации устанавливаем конечное значение
-            }
-        };
-        window.requestAnimationFrame(step);
-    };
-
-    useEffect(() => {
-        const btcValue = parseFloat(balanceToBtc) || 0;
-        const rubValue = parseFloat(balanceToRub) || 0;
-        animateValue(0, btcValue, 1000, setAnimatedBtc); // Убрали округление внутри animateValue
-        animateValue(0, rubValue, 4000, setAnimatedRub);
-    }, [balanceToBtc, balanceToRub]);
-
-    // Округляем только для отображения
-    const displayBtc = animatedBtc.toFixed(8);
-    const displayRub = animatedRub.toFixed(2);
-
     return (
         <div className='body_second'>
-            <div className='balance'>{displayBtc} BTC</div>
-            <div className='balance'>{displayRub} ₽</div>
+            <div className='balance'>
+                <CountUp end={parseFloat(balanceToBtc)} decimals={8} duration={1} /> BTC
+            </div>
+            <div className='balance'>
+                <CountUp end={parseFloat(balanceToRub)} decimals={2} duration={1} /> ₽
+            </div>
         </div>
     );
 };
