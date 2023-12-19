7fd22d19bf0434b4d420abefcdf046b297593d1b
alexjava92 <autoexpert37@yandex.ru>
2023-12-19 14:56:08 +0300

    init

--- src/components/Bitcoin/Balance/Balance.jsx
+++ src/components/Bitcoin/Balance/Balance.jsx
@@ -5,15 +5,16 @@ export const Balance = ({ balanceToBtc, balanceToRub }) => {
     const [animatedBtc, setAnimatedBtc] = useState(0);
     const [animatedRub, setAnimatedRub] = useState(0);
 
-    const animateValue = (start, end, duration, setFunction, decimals) => {
+    const animateValue = (start, end, duration, setFunction) => {
         let startTimestamp = null;
         const step = (timestamp) => {
             if (!startTimestamp) startTimestamp = timestamp;
             const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-            const animatedValue = start + progress * (end - start);
-            setFunction(Number(animatedValue.toFixed(decimals))); // Округляем до необходимого количества знаков после запятой
+            setFunction(start + progress * (end - start));
             if (progress < 1) {
                 window.requestAnimationFrame(step);
+            } else {
+                setFunction(end); // После завершения анимации устанавливаем конечное значение
             }
         };
         window.requestAnimationFrame(step);
@@ -22,15 +23,18 @@ export const Balance = ({ balanceToBtc, balanceToRub }) => {
     useEffect(() => {
         const btcValue = parseFloat(balanceToBtc) || 0;
         const rubValue = parseFloat(balanceToRub) || 0;
-        animateValue(0, btcValue, 1000, setAnimatedBtc, 8); // Указываем 8 знаков после запятой для BTC
-        animateValue(0, rubValue, 1000, setAnimatedRub, 2); // Указываем 2 знака после запятой для RUB
+        animateValue(0, btcValue, 1000, setAnimatedBtc); // Убрали округление внутри animateValue
+        animateValue(0, rubValue, 1000, setAnimatedRub);
     }, [balanceToBtc, balanceToRub]);
 
+    // Округляем только для отображения
+    const displayBtc = animatedBtc.toFixed(8);
+    const displayRub = animatedRub.toFixed(2);
 
     return (
         <div className='body_second'>
-            <div className='balance'>{animatedBtc.toFixed(8)} BTC</div>
-            <div className='balance'>{animatedRub.toFixed(2)} ₽</div>
+            <div className='balance'>{displayBtc} BTC</div>
+            <div className='balance'>{displayRub} ₽</div>
         </div>
     );
 };
