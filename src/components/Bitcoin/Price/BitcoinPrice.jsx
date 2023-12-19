f4e549ceb850306ac280d75d06bbeca3e20516b1
alexjava92 <autoexpert37@yandex.ru>
2023-12-19 10:48:16 +0300

    init

--- src/components/Bitcoin/Price/BitcoinPrice.jsx
+++ src/components/Bitcoin/Price/BitcoinPrice.jsx
@@ -15,7 +15,7 @@ export const BitcoinPrice = () => {
     }, []);
 
     return (
-        <div className="bitcoin-price">
+        <div className={'body_second'}>
             <h2>Курс Биткоина</h2>
             {btcPrice.USD && <p>1 BTC = {btcPrice.USD.last} USD</p>}
             {btcPrice.RUB && <p>1 BTC = {btcPrice.RUB.last} RUB</p>}
