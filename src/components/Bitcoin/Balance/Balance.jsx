362d44b8f0db7c4b625fc5a73b9121cfbd29deeb
alexjava92 <autoexpert37@yandex.ru>
2023-12-19 14:50:51 +0300

    init

--- src/components/Bitcoin/Balance/Balance.jsx
+++ src/components/Bitcoin/Balance/Balance.jsx
@@ -5,12 +5,13 @@ export const Balance = ({ balanceToBtc, balanceToRub }) => {
     const [animatedBtc, setAnimatedBtc] = useState(0);
     const [animatedRub, setAnimatedRub] = useState(0);
 
-    const animateValue = (start, end, duration, setFunction) => {
+    const animateValue = (start, end, duration, setFunction, decimals) => {
         let startTimestamp = null;
         const step = (timestamp) => {
             if (!startTimestamp) startTimestamp = timestamp;
             const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-            setFunction(start + progress * (end - start));
+            const animatedValue = start + progress * (end - start);
+            setFunction(Number(animatedValue.toFixed(decimals))); // Округляем до необходимого количества знаков после запятой
             if (progress < 1) {
                 window.requestAnimationFrame(step);
             }
@@ -18,14 +19,13 @@ export const Balance = ({ balanceToBtc, balanceToRub }) => {
         window.requestAnimationFrame(step);
     };
 
-
     useEffect(() => {
-        console.log("API BTC:", balanceToBtc, "API RUB:", balanceToRub); // Для отладки
         const btcValue = parseFloat(balanceToBtc) || 0;
         const rubValue = parseFloat(balanceToRub) || 0;
-        animateValue(0, btcValue, 1000, setAnimatedBtc);
-        animateValue(0, rubValue, 1000, setAnimatedRub);
+        animateValue(0, btcValue, 1000, setAnimatedBtc, 8); // Указываем 8 знаков после запятой для BTC
+        animateValue(0, rubValue, 1000, setAnimatedRub, 2); // Указываем 2 знака после запятой для RUB
     }, [balanceToBtc, balanceToRub]);
+
 
     return (
         <div className='body_second'>
