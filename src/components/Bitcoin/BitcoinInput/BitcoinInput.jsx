b684ce69d800e3a24136f2dee44abe2379b99cfd
alexjava92 <autoexpert37@yandex.ru>
2023-12-23 13:09:27 +0300

    init

--- src/components/Bitcoin/BitcoinInput/BitcoinInput.jsx
+++ src/components/Bitcoin/BitcoinInput/BitcoinInput.jsx
@@ -118,7 +118,7 @@ export const BitcoinInput = ({
             setLastUpdatedByUserBitcoin(false);
         };
         convert();
-    }, [bitcoinAmount, lastUpdatedByUserBitcoin, showUsd]);
+    }, [bitcoinAmount, lastUpdatedByUserBitcoin]);
 
     useEffect(() => {
         const convert = async () => {
@@ -134,7 +134,7 @@ export const BitcoinInput = ({
             setLastUpdatedByUserRub(false);
         };
         convert();
-    }, [rubAmount, usdAmount, lastUpdatedByUserRub, showUsd]);
+    }, [rubAmount, usdAmount, lastUpdatedByUserRub]);
 
     useEffect(() => {
         const validateAddress = async () => {
