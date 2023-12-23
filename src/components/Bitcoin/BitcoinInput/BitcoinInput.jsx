fbc7507290c6b1c313793c9aa7ad175c8451e8d9
alexjava92 <autoexpert37@yandex.ru>
2023-12-23 13:24:23 +0300

    init

--- src/components/Bitcoin/BitcoinInput/BitcoinInput.jsx
+++ src/components/Bitcoin/BitcoinInput/BitcoinInput.jsx
@@ -62,6 +62,8 @@ export const BitcoinInput = ({
         console.log(inputValue);
         if(inputValue === ''){
             setBitcoinAmount('');
+            setUsdAmount('');
+            setRubAmount('');
             return;
         }
 
