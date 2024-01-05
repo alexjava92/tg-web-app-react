ebd29170d62db82e81701808912e2b7ee31d3077
alexjava92 <autoexpert37@yandex.ru>
2024-01-05 15:34:07 +0300

    init

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    apiBaseUrl: 'https://abc0-38-54-29-177.ngrok-free.app',
+    apiBaseUrl: 'https://53a9-146-70-114-2.ngrok-free.app',
     mempoolUrl: 'https://mempool.space/testnet',
 };
 
